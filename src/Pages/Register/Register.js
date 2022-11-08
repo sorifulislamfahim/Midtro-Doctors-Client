fcf08364c2bcc62987bd8f99836de7dd8a5f7e3f
Soriful Islam Fahim <islamsoriful248@gmail.com>
2022-11-08 21:32:50 +0600

    added Authentication and register part setup

---
+++ src/Pages/Register/Register.js
@@ -0,0 +1,62 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import login from '../../assets/login/login.png'
+import { AuthContext } from '../../Context/Authprovider/Authprovider';
+
+const Register = () => {
+    const { createUser } = useContext(AuthContext);
+
+    const handleRegister = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        createUser(email, password)
+        .then(result => {
+            const user = result.user;
+            console.log(user)
+        }) 
+        .catch(error => console.error(error))
+
+    }
+
+    return (
+        <div className="hero my-32">
+        <div className="hero-content grid md:grid-cols-2 flex-col lg:flex-row">
+            <div className="text-center lg:text-left">
+                <img src={login} alt="" />
+            </div>
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
+                <h1 className="text-5xl font-bold text-center">Register</h1>
+                <form onSubmit={handleRegister} className="card-body">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" name='name' placeholder="Your Name" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="text" name='email' placeholder="email" className="input input-bordered" required/>
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="text" name='password' placeholder="password" className="input input-bordered" required/>
+                    </div>
+                    <div className="form-control mt-6">
+                        <input type="submit" className="btn btn-primary" value="Register" />
+                    </div>
+                </form>
+                <p className='text-center'>Alredy Have an acount Please <Link className='text-orange-600 font-bold' to='/login'>LogIn</Link> </p>
+            </div>
+        </div>
+    </div>
+    );
+};
+
+export default Register;
